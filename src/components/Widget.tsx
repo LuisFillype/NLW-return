3af06a7ced10dbd1aa4464e66cfa3aebe5c82c8d
LuisFillype <l.fillype@gmail.com>
2022-05-12 10:50:37 -0300

    refactor: update widget style

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,6 +1,5 @@
-import {ChatTeardropDots} from 'phosphor-react'
-import { useState } from 'react'
-import {Popover} from '@headlessui/react'
+import { Popover } from '@headlessui/react'
+import { ChatTeardropDots } from 'phosphor-react'
 export function Widget(){
     
     return (
